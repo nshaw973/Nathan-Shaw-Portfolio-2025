f36ca25eaae550d918229467e62737f008f157fa
Nathan Shaw <nshaw973@gmail.com>
2025-03-05 14:52:52 -0800

    gh

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import { Navbar, Footer } from "./components";
 
 function App() {
   return (
-    <BrowserRouter basename="/Nathan-Shaw-Portfolio-2025">
       <div className="App min-w-svw min-h-svh flex flex-col bg-gradient-to-tr from-slate-900 to-neutral-500 bg-fixed  overflow-y-hidden ">
         <Router>
           <Navbar />
@@ -27,7 +26,6 @@ function App() {
           <Footer />
         </Router>
       </div>
-    </BrowserRouter>
   );
 }
 
