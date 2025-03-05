e17a01228c210da7bea306d3e45502dc50dc778b
Nathan Shaw <nshaw973@gmail.com>
2025-03-05 15:49:33 -0800

    Fixed routing and basename

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Homepage, About, Projects, Contact, Resume } from "./pages";
 import { Navbar, Footer } from "./components";
 
 function App() {
+  // Set basename dynamically
+  const basename = process.env.NODE_ENV === 'production' ? '/Nathan-Shaw-Portfolio-2025' : '/';
+
   return (
-    <BrowserRouter basename={"/Nathan-Shaw-Portfolio-2025"}>
-      <div className="App min-w-svw min-h-svh flex flex-col bg-gradient-to-tr from-slate-900 to-neutral-500 bg-fixed  overflow-y-hidden ">
-        <Router>
-          <Navbar />
-          <div className="flex flex-grow w-full ml-auto mr-auto ">
-            <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/resume" element={<Resume />} />
-            </Routes>
-          </div>
-          <Footer />
-        </Router>
+    <BrowserRouter basename={basename}>
+      <div className="App min-w-svw min-h-svh flex flex-col bg-gradient-to-tr from-slate-900 to-neutral-500 bg-fixed overflow-y-hidden">
+        <Navbar />
+        <div className="flex flex-grow w-full ml-auto mr-auto">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </BrowserRouter>
-
   );
 }
 
